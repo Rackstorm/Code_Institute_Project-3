68ed151e8a8c74ff4b70d1b27e3b1f1cd9875271
Rackstorm <>
2023-07-25 21:11:42 +0000

    setting up basic structure for game

--- run.py
+++ run.py
@@ -3,12 +3,17 @@
 import random
 
 def creating_board(rows, cols)
+""" creating the board consisting of a grid the user can modify themselves """
 
 def print_board(board)
+""" printing out the grid chosen """
 
 def placing_ships(board, ship_size)
+""" the users chooses how many ships to put on the board """
 
 def get_guess(rows, cols):
+    """ the user types in their guesses """
 
 def play_game(rows, cols, num_ships):
+    """ puts the ships randomly on the board, keeps track of hits/misses and keeps going until the user has sunk all the ships"""
 
