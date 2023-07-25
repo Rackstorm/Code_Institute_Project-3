73027c159d679c5912bf96ae2a727badbefda1b2
Rackstorm <>
2023-07-25 21:05:58 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" Battleship Game """
+
+import random
+
+def creating_board(rows, cols)
+
+def print_board(board)
+
+def placing_ships(board, ship_size)
+
+def get_guess(rows, cols):
+
+def play_game(rows, cols, num_ships):
+
