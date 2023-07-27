6bdf26001722b04a0488d136d38b80f1518008ec
Rackstorm <>
2023-07-27 09:29:39 +0000

    Structuring up the different functions as well as adding comments

--- run.py
+++ run.py
@@ -1,60 +1,60 @@
 import random
 
-""" Creating the grid """
-
-
 def create_board(rows, cols):
-    return [['O' for _ in range(cols)] for _ in range(rows)]
-
-
-""" printing out the board in the terminal """
-
+    """
+    creates a game board 10x10 rows/columns.
+    all empty cells are marked with an 'O'
+    """
+    board = []
+    for _ in range(rows):
+        row = ['O'] * cols
+        board.append(row)
+    return board
 
 def print_board(board):
+    """
+    prints the game board
+    """
     for row in board:
         print(" ".join(row))
 
-
-""" placing the ships randomly on the board horizontally or vertically"""
-
-
 def place_ship(board, ship_size):
-    rows = len(board)
-    cols = len(board[0])
+    """
+    randomly places a ship on the board.
+    size can differ from 1 to 4.
+    ships can be placed horizontally or vertically.
+    """
+    rows, cols = len(board), len(board[0])
     orientation = random.choice(['horizontal', 'vertical'])
 
-    if orientation == 'horizontal':
-        row = random.randint(0, rows - 1)
-        col = random.randint(0, cols - ship_size)
-        for i in range(ship_size):
+    row, col = random.randint(0, rows - ship_size), random.randint(0, cols - ship_size)
+
+    for i in range(ship_size):
+        if orientation == 'horizontal':
             board[row][col + i] = 'S'
-    else:
-        row = random.randint(0, rows - ship_size)
-        col = random.randint(0, cols - 1)
-        for i in range(ship_size):
+        else:
             board[row + i][col] = 'S'
 
-
-""" the user types in their guess """
-
-
 def get_guess(rows, cols):
+    """
+    asks the player to input their guess (e.g., A5).
+    converts the input into row and column indices on the board.
+    """
     while True:
         try:
-            guess = input("Enter your guess (e.g., A5): \n")
+            guess = input("Enter your guess (e.g., A5): " n\).strip().upper()
             col, row = ord(guess[0]) - ord('A'), int(guess[1:]) - 1
             if 0 <= row < rows and 0 <= col < cols:
                 return row, col
             else:
-                print("Invalid guess. Try again.\n")
+                print("Invalid guess. Try again." n\)
         except (ValueError, IndexError, TypeError):
-            print("Invalid guess. Try again.\n")
-
-
-""" the main function calling the board and ships function and loops until game over or user winning"""
-
+            print("Invalid guess. Try again." n\)
 
 def play_battleship(rows, cols, num_ships):
+    """
+    main function to play the game.
+    """
     board = create_board(rows, cols)
     ships_placed = 0
 
@@ -62,12 +62,11 @@ def play_battleship(rows, cols, num_ships):
         place_ship(board, random.randint(1, 4))
         ships_placed += 1
 
-    print("Welcome to Battleship!\n")
-    print("You have 5 attempts to try and sink the battleships!\n")
+    print("Welcome to Battleship!" n\)
+    print("You have 5 attempts to try and sink the battleships!"n\)
     print_board(board)
 
-    attempts = 0
-    max_attempts = 5
+    attempts, max_attempts = 0, 5
 
     while attempts < max_attempts:
         attempts += 1
@@ -84,15 +83,16 @@ def play_battleship(rows, cols, num_ships):
             print_board(board)
 
         if num_ships == 0:
-            print("Good job! You sank all the battleships!\n")
+            print("Good job! You sank all the battleships!")
             break
     else:
         print("Game Over! Try again.")
 
+def main():
+    rows, cols = 10, 10
+    num_ships = 4
+    play_battleship(rows, cols, num_ships)
 
 if __name__ == "__main__":
-    rows = 10
-    cols = 10
-    num_ships = min(
-        int(input("Enter the number of ships: \n")), rows * cols // 5)
-    play_battleship(rows, cols, num_ships)
+    """is called when program is run from terminal or PyCharms"""
+    main()
