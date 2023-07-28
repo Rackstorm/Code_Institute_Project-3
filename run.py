1948585edc7d6f168ca8e4992966ee6548f1acf0
Rackstorm <>
2023-07-28 20:32:32 +0000

    adding variables I accidentally deleted and fixing ships and board game grid

--- run.py
+++ run.py
@@ -21,17 +21,18 @@ def print_board(board, game_over=False):
     print(grid_reference(len(board[0])))
 
     for i, row in enumerate(board):
-        display_row = ['O']
+        display_row = [grid_reference(1)[i]]
         for cell in row:
             if cell == 'S' and not game_over:
                 display_row.append('O')
             else:
                 display_row.append(cell)
-        print(f"{i + 1:2} {' '.join(display_row)}")
+        print(" ".join(display_row))
 
 
 def grid_reference(cols):
-    return "  " + " ".join(chr(65 + i) for i in range(cols))
+    letters = 'ABCDEFGHIJ'
+    return "  " + " ".join(letters[:cols])
 
 
 def place_ship(board, ship_size):
@@ -42,6 +43,7 @@ def place_ship(board, ship_size):
     """
     rows, cols = len(board), len(board[0])
     orientation = random.choice(['horizontal', 'vertical'])
+    ship_size = random.randint(1, 4)
     row, col = random.randint(0, rows - 1), random.randint(0, cols - 1)
 
     for i in range(ship_size):
@@ -60,14 +62,14 @@ def get_guess(rows, cols):
     """
     while True:
         try:
-            guess = input("Enter your guess (e.g., A5): ").strip().upper()
+            guess = input("Enter your guess (e.g., A5):  \n").strip().upper()
             col, row = ord(guess[0]) - ord('A'), int(guess[1:]) - 1
             if 0 <= row < rows and 0 <= col < cols:
                 return row, col
             else:
-                print("Invalid guess. Try again.")
+                print("Invalid guess. Try again. \n")
         except (ValueError, IndexError, TypeError):
-            print("Invalid guess. Try again.")
+            print("Invalid guess. Try again. \n")
 
 
 def play_battleship(rows, cols, num_ships):
@@ -81,8 +83,8 @@ def play_battleship(rows, cols, num_ships):
         place_ship(board, random.randint(1, 4))
         ships_placed += 1
 
-    print("Welcome to Battleship!")
-    print("You have 5 attempts to try and sink the battleships!")
+    print("Welcome to Battleship! \n")
+    print("You have 5 attempts to try and sink the battleships! \n")
     print_board(board)
 
     attempts, max_attempts = 0, 5
@@ -92,13 +94,13 @@ def play_battleship(rows, cols, num_ships):
         row, col = get_guess(rows, cols)
 
         if board[row][col] == 'S':
-            print("Hit!")
+            print("Hit! \n")
             board[row][col] = 'X'
             num_ships -= 1
             print_board(board)
 
             if num_ships == 0:
-                print("Good job! You sank all the battleships!")
+                print("Good job! You sank all the battleships! \n")
                 print_board(board, game_over=True)
                 break
         else:
@@ -106,12 +108,12 @@ def play_battleship(rows, cols, num_ships):
             board[row][col] = ':'
             print_board(board)
     else:
-        print("Game Over! Try again.")
+        print("Game Over! Try again. \n")
 
 
 def main():
     rows, cols = 10, 10
-    num_ships = 3
+    num_ships = 5
     play_battleship(rows, cols, num_ships)
 
 
