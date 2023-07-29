2f56a13b44be6c9dacd88e6331df72963e9ad2e1
Rackstorm <>
2023-07-29 10:09:31 +0000

    Resolving reference bug, adding new functions such as start game and display game rules

--- run.py
+++ run.py
@@ -52,7 +52,7 @@ def print_board(board, game_over=False):
 
 
 def grid_reference(cols):
-    return "  " + " ".join(chr(65 + i) for i in range(cols))
+    return "   " + " ".join(chr(65 + i) for i in range(cols + 1))
 
 
 def place_ship(board, ship_size):
@@ -70,8 +70,7 @@ def place_ship(board, ship_size):
             board[row][col + i] = 'S'
         elif orientation == 'vertical' and row + ship_size <= rows:
             board[row + i][col] = 'S'
-        else:
-            place_ship(board, ship_size)
+            break
 
 
 def get_guess(rows, cols):
@@ -132,10 +131,43 @@ def play_battleship(rows, cols, num_ships):
         print("Game Over! Try again. \n")
 
 
-def main():
-    rows, cols = 10, 10
-    num_ships = 5
+def start_battleship():
+    rows, cols = 10, 9
+    num_ships = 80
     play_battleship(rows, cols, num_ships)
+    clear_screen()
+
+
+def display_game_rules():
+    print("=== Battleship Game Rules === \n")
+    print("In this very simple game you only have to strike down the ships.\n")
+    print("1. You have x attempts to sink the ships.")
+    print("2. You enter your guess by typing col + row (e.g. A3)")
+    print("3. If you miss, the board will mark it with a '/'")
+    print("4. If you hit, the board will mark the cell with an 'X'")
+    print("5. If you run out of attempts, it's game over.")
+    print("6. If you win, the game score will show.\n")
+
+
+def main():
+
+    while True:
+        print("=== Battleship Game === \n")
+        print("1. Play Game")
+        print("2. Game Rules")
+        print("3. Exit \n")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            start_battleship()
+        elif choice == '2':
+            display_game_rules()
+        elif choice == '3':
+            print("Exiting Battleship Game. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
 
 
 if __name__ == "__main__":
